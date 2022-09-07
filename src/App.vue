25ee90c841958b3bbc24138fd3ca410aeec1fecb
seta <seta_shinsuke@qq.com>
2022-09-08 02:33:07 +0800

    可以正确路由了（终于）

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--  <img alt="Vue logo" src="./assets/logo.png">-->
+    <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <div id="app_in">
+        <h1>Hello App!</h1>
+        <p>
+            <!--使用 router-link 组件进行导航 -->
+            <!--通过传递 `to` 来指定链接 -->
+            <!--`<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签-->
+            <router-link to="/hello">Go to Hello</router-link>
+            <router-link to="/manga_detail">Go to Manga Detail</router-link>
+        </p>
+        <!-- 路由出口 -->
+        <!-- 路由匹配到的组件将渲染在这里 -->
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    // import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        // name: 'App',
+        // components: {
+        //   HelloWorld
+        // }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
+// 构建工具-配置
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // publicPath: '/', // default: '/'
+  publicPath: getPublicPath(), // default: '/'
+  outputDir: 'dist', // default: 'dist'
+  assetsDir: 'assets', // default: ''
+  indexPath: 'index.html', // default: 'index.html'
+  // devServer: {
+  //   proxy: 'http://192.168.50.96:9292'
+  // }
 })
+
+// 配置构建文件目录, 默认在根目录，打包的时候设定为当前目录
+function getPublicPath() {
+  switch (process.env.NODE_ENV) {
+    case 'production': return './'
+    case 'dev': return '/'
+    default: return '/'
+  }
+}
